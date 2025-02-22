b4c95efa01912b33835ab2c6d906ed95144d2878
RyuuseiAkira <haokhanh713@gmail.com>
2025-02-22 23:18:46 +0700

    Initial commit

--- private_dot_local/share/bin/executable_temp.sh
+++ private_dot_local/share/bin/executable_temp.sh
@@ -7,13 +7,13 @@ green='\033[32m'
 reset='\033[0m'  # Reset color 
 
 # Get temp
-cputemp=$(cpuinfo.sh | jq -r '.text')
+cputemp=$(cpuinfo.sh --use amd | jq -r '.text' | grep -oP '\d+°C')
 gputemp=$(gpuinfo.sh --use amd | jq -r '.text' | grep -oP '\d+°C')
 
-#temp
+#decor cause i dont like minimalist :D
 echo "┌─Temperatures─────┐"
 echo
-echo -e "${red}    Cpu:${reset} $cputemp" 
+echo -e "${red}    Cpu:${reset}  $cputemp" 
 echo -e "${green}    Gpu:${reset}  $gputemp"
 echo
 echo "└──────────────────┘"
@@ -22,13 +22,13 @@ echo
 
 # Check cooler boost state
 current_state=$(cat "$cooler_boost_file")
-
+# print current stage
 if [[ "$current_state" == "on" ]]; then
   echo -e "Current cooler boost state: ${green}ON${reset}"
 else
   echo -e "Current cooler boost state: ${red}OFF${reset}"
 fi
-
+# meow
 echo
 echo "Enter and type password to toggle cooler boost"
 read -n1 -s -r
