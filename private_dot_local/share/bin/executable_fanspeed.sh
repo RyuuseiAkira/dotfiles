1c5cdf9f1cabbf557c006599a4bae51acb757c59
Akira <haokhanh713@gmail.com>
2025-06-01 18:34:22 +0700

    Initial commit

--- private_dot_local/share/bin/executable_fanspeed.sh
+++ private_dot_local/share/bin/executable_fanspeed.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-# extract temperature from output
-extract_temp() {
-  "$@" | jq -r '.text' | grep -oP '\d+°C' | grep -oP '\d+'
-}
-
-# send notification
-send_notification() {
-  notify-send -a "Power" -u critical "Temperature" "$1 temperature is $2°C! Please turn on turbo fan"
-}
-
-# Check CPU temperature
-cpu_temp=$(extract_temp cpuinfo.sh --use amd)
-
-# Check GPU temperature
-gpu_temp=$(extract_temp gpuinfo.sh --use amd)
-
-# Check if either CPU or GPU temperature
-if [ -n "$cpu_temp" ] && [ "$cpu_temp" -gt 85 ]; then
-  send_notification "CPU" "$cpu_temp"
-elif [ -n "$gpu_temp" ] && [ "$gpu_temp" -gt 85 ]; then
-  send_notification "GPU" "$gpu_temp"
-fi
-
 #waybar stuff
 # Check cooler boost status
 cooler_boost_status=$(cat /sys/devices/platform/msi-ec/cooler_boost)
---
+++ private_dot_local/share/bin/executable_obs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p ~/.config/obs-studio/themes
+git clone https://github.com/catppuccin/obs.git
+cp -r ./obs/themes/ ~/.config/obs-studio/
+rm -rf ./obs/
+#color.set.sh --single ~/.config/hyde/wallbash/always/obs.dcol
+echo "To apply the theme:"
+echo "- Restart OBS"
+echo "- Go into the Settings"
+echo "- Go into the Appearance tab"
+echo "- Choose the 'Wallbash' style"
